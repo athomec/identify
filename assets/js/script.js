be98aa8cf59903cd33212d2776489973ec7b433b
vagabond <athomec@pie.com.tw>
2023-11-21 14:15:18 +0800

    測試調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 $(function () {//JS開頭
-
-	var WINDOW = $(window).width();//視窗寬度
-	var WINDOWH = $(window).height();//視窗高度
-
 	//按鈕切換狀態
 	$(".js-toggle-btn").click(function () {
 		$(this).toggleClass('active');
 	})
-	
+
+	//平面圖按鈕
+	$(".js-tool-menu").find("button").click(function () {
+		if ($(this).hasClass("active")) {
+			$(this).removeClass("active");
+		} else {
+			$(this).addClass("active");
+			// 移除其他按鈕的 active 類
+			$(".js-tool-menu").find("button").not(this).removeClass("active");
+		}
+	});
 })//JS尾端	
