9cc52c18d384a32751983f6ed491abada3119865
vagabond <athomec@pie.com.tw>
2023-11-21 10:46:09 +0800

    上傳檔案

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(function () {//JS開頭
+
+	var WINDOW = $(window).width();//視窗寬度
+	var WINDOWH = $(window).height();//視窗高度
+
+	//按鈕切換狀態
+	$(".js-toggle-btn").click(function () {
+		$(this).toggleClass('active');
+	})
+	
+})//JS尾端	
